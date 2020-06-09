6cf1f0b555914dad19ede1e3c7ba8ab1372311db
Ardalan Amini <ardalanamini22@gmail.com>
2020-06-09 23:09:32 +0430

    Update README.md

--- README.md
+++ README.md
@@ -1,13 +1,70 @@
 # messenger-bot
 
-## Documents
+## Table of Contents
 
-```bash
+- [Installation](#installation)
+- [Usage](#usage)
+    - [Basic](#basic)
+    - [Docker](#docker)
+    - [Tunneling](#tunneling)
+- [Documents](#documents)
+
+### Installation
+
+First install the dependencies:
+
+```shell script
+npm i
+```
+
+Then build the project:
+
+```shell script
+npm run build
+```
+
+In the end, specify the required environment variables from the Facebook developer panel:
+
+```shell script
+cp .env.example .env
+```
+
+- `BOT_ID`: The `App ID` from `Settings`->`Basic`
+- `BOT_SECRET`: The `App Secret` from `Settings`->`Basic`
+- `BOT_PAGE_ID`: The `Page ID` from `Messenger`->`Settings`->`Access Tokens`
+- `BOT_PAGE_TOKEN`: The `Page Token` from `Messenger`->`Settings`->`Access Tokens`
+- `BOT_VERIFY_TOKEN`: The `Verify Token` from `Messenger`->`Settings`->`Webhooks`
+
+### Usage
+
+#### Basic
+
+```shell script
+npm run start
+```
+
+#### Docker
+
+```shell script
+docker-compose up -d
+```
+
+Then use the following commands to be able to connect using application webhooks
+
+```shell script
+npm run tunnel
+```
+
+or using [ngrok](https://ngrok.com)
+
+```shell script
+ngrok http 3000
+```
+
+### Documents
+
+```shell script
 npm run document
 ```
 
-## Docker
-
-```bash
-docker-compose up -d
-```
+After running the command, go to [http://localhost:8080](http://localhost:8080) on your preferred browser
--- src/controllers/message/get.ts
+++ src/controllers/message/get.ts
@@ -1,10 +1,37 @@
 import { Message } from "@bot/models";
 import { RequestHandler } from "express";
 
-const CONTROLLER: RequestHandler = async function get(req, res) {
-  const messages = await Message.find().populate("user");
+export interface Query {
+  page?: number;
+  limit?: number;
+}
 
-  res.json({ messages });
+const CONTROLLER: RequestHandler<any, any, any, Query> = async function get(req, res) {
+  let messages: Message.SCHEMA[] = [];
+  let { page = 0, limit = 10 } = req.query;
+
+  page = +page;
+  limit = +limit;
+
+  const total = await Message.countDocuments();
+
+  if (total > 0) {
+    messages = await Message
+      .find()
+      .skip(page * limit)
+      .limit(limit)
+      .populate("user");
+  }
+
+  res.json({
+    messages,
+    meta: {
+      page,
+      limit,
+      page_count: messages.length,
+      total_count: total,
+    },
+  });
 };
 
 export default CONTROLLER;
@@ -17,6 +44,21 @@ export default CONTROLLER;
  *     tags:
  *       - Message
  *     description: Get messages
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         description: "Zero based pagination page"
+ *         schema:
+ *           type: integer
+ *           minimum: 0
+ *           default: 0
+ *       - in: query
+ *         name: limit
+ *         description: "Pagination limit"
+ *         schema:
+ *           type: integer
+ *           minimum: 0
+ *           default: 10
  *     produces:
  *       - application/json
  *     responses:
@@ -28,9 +70,33 @@ export default CONTROLLER;
  *               type: object
  *               required:
  *                 - messages
+ *                 - meta
  *               properties:
  *                 messages:
  *                   type: array
  *                   items:
  *                     $ref: "#/definitions/Message"
+ *                     type: object
+ *                 meta:
+ *                   type: object
+ *                   required:
+ *                     - page
+ *                     - limit
+ *                     - page_count
+ *                     - total_count
+ *                   properties:
+ *                     page:
+ *                       type: integer
+ *                       minimum: 0
+ *                       default: 0
+ *                     limit:
+ *                       type: integer
+ *                       minimum: 0
+ *                       default: 10
+ *                     page_count:
+ *                       type: integer
+ *                       minimum: 0
+ *                     total_count:
+ *                       type: integer
+ *                       minimum: 0
  */
